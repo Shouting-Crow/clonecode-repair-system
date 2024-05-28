7c74b8c494e9a3d0e904e17a7d426f3834c98f39
Shouting-Crow <jinwook5062@gmail.com>
2024-05-28 16:31:11 +0900

    [1]프로젝트 초기 단계

---
+++ src/main/java/com/clonecode/repairweb/RepairWebApplication.java
@@ -0,0 +1,13 @@
+package com.clonecode.repairweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepairWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clonecode/repairweb/RepairWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonecode.repairweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
